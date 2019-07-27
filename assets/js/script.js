39de3bf1c5840274f10f19b588efbf079f8e93c0
yesenia <yess992@gmail.com>
2019-07-27 12:07:39 -0400

    mi quinta cagada

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
-})
+
 
   $('[data-toggle="popover"]').popover();
 
 });
+
+$("a").click(function(event){
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var gato = this.hash;
+
+      $("html, body").animate({
+        scrollTop: $(gato).offset().top
+      }, 800, function(){
+        window.location.hash = gato;
+      });
+    }
+})
